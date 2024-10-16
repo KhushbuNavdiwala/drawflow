70e95e2a457e0eb8a4382a1e60ba00e1a4feaac8
Khushbu Navdiwala <navdiwala.khushbu@gmail.com>
2024-10-16 19:15:57 +0200

    solve error for styling

--- my-react-flow-app/src/App.tsx
+++ my-react-flow-app/src/App.tsx
@@ -1,16 +1,23 @@
 import { ReactFlow, Controls, Background } from '@xyflow/react'
+import '@xyflow/react/dist/style.css'
 
-import './App.css'
 
-function App() {
+function Flow() {
  
 
   return (
 
-    <></>
+    <div style={{ width: '100%', height: '100%' }}>
+
+      <ReactFlow style={{ width: '100%', height: '100%' }}>
+      <Background/>
+      <Controls/>
+      </ReactFlow>
+
+    </div>
     
    
   )
 }
 
-export default App
+export default Flow;
